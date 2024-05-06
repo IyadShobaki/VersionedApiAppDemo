be707bd6951f866bcbc6e3f6d2e3cc88e80a3a43
Iyad Shobaki <iyadshobaki82@gmail.com>
2024-05-05 20:54:42 -0400

    Version Neutral Endpoints  - Created controller to check the health of the API

---
+++ VersionedApi/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+// Version Neutral Endpoints   (Version independent) 
+namespace VersionedApi.Controllers
+{
+   // We put th version in the url but it doesn't matter the version we choose
+   // It will give you the health of the API
+   [Route("api/v{version:apiVersion}/[controller]")]
+   [ApiController]
+   [ApiVersionNeutral]
+   public class HealthController : ControllerBase
+   {
+      [HttpGet]
+      [Route("ping")]
+      public IActionResult Ping()
+      {
+         return Ok("Everything seems great!");
+      }
+   }
+}
